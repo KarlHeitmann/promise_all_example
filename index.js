3d26fc226baec326d8e5546c0bb70b8c316fe54a
Karl Heitmann <khf1988@gmail.com>
2022-08-10 14:49:59 -0400

    added axios, call to getonbrd API

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
+const axios = require('axios').default;
+
+axios.get('https://www.getonbrd.com/api/v0/categories?per_page=10&page=1')
+  .then(function ({data}) {
+    // handle success
+    console.log(data.data);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .then(function () {
+    // always executed
+  });
+
+
 // A simple promise that resolves after a given time
 const timeOut = (t) => {
   return new Promise((resolve, reject) => {
