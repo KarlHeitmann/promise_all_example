245b917ea1dcb4556f8ecb2dc2b197f56f0205c9
Karl Heitmann <khf1988@gmail.com>
2022-08-10 14:52:33 -0400

    creadas funciones

--- index.js
+++ index.js
@@ -1,32 +1,38 @@
 const axios = require('axios').default;
 
-axios.get('https://www.getonbrd.com/api/v0/categories?per_page=10&page=1')
-  .then(function ({data}) {
-    // handle success
-    console.log(data.data);
-  })
-  .catch(function (error) {
-    // handle error
-    console.log(error);
-  })
-  .then(function () {
-    // always executed
-  });
+function main() {
+  axios.get('https://www.getonbrd.com/api/v0/categories?per_page=10&page=1')
+    .then(function ({data}) {
+      // handle success
+      console.log(data.data);
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    })
+    .then(function () {
+      // always executed
+    });
+}  
 
 
-// A simple promise that resolves after a given time
-const timeOut = (t) => {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve(`Completed in ${t}`)
-    }, t)
-  })
+function ejemplo() {
+  // A simple promise that resolves after a given time
+  const timeOut = (t) => {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(`Completed in ${t}`)
+      }, t)
+    })
+  }
+  
+  // Resolving a normal promise.
+  timeOut(1000)
+    .then(result => console.log(result)) // Completed in 1000
+  
+  // Promise.all
+  Promise.all([timeOut(1000), timeOut(2000)])
+    .then(result => console.log(result)) // ["Completed in 1000", "Completed in 2000"]
 }
 
-// Resolving a normal promise.
-timeOut(1000)
-  .then(result => console.log(result)) // Completed in 1000
-
-// Promise.all
-Promise.all([timeOut(1000), timeOut(2000)])
-  .then(result => console.log(result)) // ["Completed in 1000", "Completed in 2000"]
+main()
