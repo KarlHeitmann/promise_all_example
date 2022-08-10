80590efb8e1412f1f67ae6eb5c842e69b6766038
Karl Heitmann <khf1988@gmail.com>
2022-08-10 15:07:42 -0400

    agregado funcion trabajosConsecutivos que solo muestra los ids de categorias, y hace un llamado para tomar los trabajos de una categoria

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 const axios = require('axios').default;
 
+function trabajosConsecutivos(categories) {
+  for (let i = 0; i < categories.length; i++) {
+    const category = categories[i];
+    console.log(category.id)
+  }
+}
+
 function main() {
   axios.get('https://www.getonbrd.com/api/v0/categories?per_page=10&page=1')
     .then(function ({data}) {
       // handle success
-      console.log(data.data);
+      trabajosConsecutivos(data.data)
+      // axios.get('https://www.getonbrd.com/api/v0/categories/programming/jobs?per_page=1&page=1')
+      const category_id = 'programming'
+      axios.get(`https://www.getonbrd.com/api/v0/categories/${category_id}/jobs?per_page=1&page=1&expand=["company"]`)
+        .then(function ({data}) {
+        // .then(function (response) {
+          console.log(data)
+        })
     })
     .catch(function (error) {
       // handle error
